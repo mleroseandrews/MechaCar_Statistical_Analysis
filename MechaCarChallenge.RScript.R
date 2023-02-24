bc639f60ffd3e65619f651588611dcd6171d3cb1
Emily Andrews <116030873+mleroseandrews@users.noreply.github.com>
2023-02-24 10:11:23 -0600

    Suspension Coil

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,3 +3,7 @@ data_mpg <- read.csv('MechaCar_mpg.csv')
 colnames(data_mpg)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = data_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = data_mpg))
+
+suspension <-read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- summarize(suspension,mean= mean(PSI),median = median(PSI),variance=var(PSI),SD = sd(PSI),n = n())
+lot_summary <-suspension %>% group_by(Manufacturing_Lot) %>% summarize(mean= mean(PSI),median = median(PSI),variance=var(PSI),SD = sd(PSI),n = n(),.groups = 'keep')
